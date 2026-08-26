49fe9cfa9b62716443362075d34e9fc2263bea77
Prakash Giri <prakashgiri20540811@gmail.com>
2026-08-26 19:53:20 +0545

    user login, signup added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoute")
 dotenv.config();
 
 connectDB();
@@ -14,6 +15,7 @@ app.get("/", (req, res) => {
     res.send(" backend is running...")
 });
 
+app.use("/api/auth", authRoutes)
 
 
 const PORT = process.env.PORT || 3000;
