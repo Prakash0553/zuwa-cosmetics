1581fb917d4116f2a87d0d725fb41b182b3bd873
Prakash Giri <prakashgiri20540811@gmail.com>
2026-08-30 10:10:56 +0545

    add blog, and product detail page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,26 +1,26 @@
-// import ChooseUs from "../components/ChooseUs"
-// import Banner from "../components/Banner"
-// import ProductHighlight from "../components/ProductHighlight"
-// import BrowseCategory from "../components/Category";
-// import Explore from "../components/Explore";
-// import NoHeader from "../components/NoHeader";
-// import NewArrival from "../components/NewArrival"
+import ChooseUs from "../components/ChooseUs"
+import Banner from "../components/Banner"
+import ProductHighlight from "../components/ProductHighlight"
+import BrowseCategory from "../components/Category";
+import Explore from "../components/Explore";
+import NoHeader from "../components/NoHeader";
+import NewArrival from "../components/NewArrival"
 import Blogs from "../components/Blogs";
-import ShoppingCart from "../components/Cart";
+//import ShoppingCart from "../components/Cart";
 import Review from "../components/Reviews";
 
 
 const Home = () => {
   return (
     <>
-      {/* <Banner />
+      <Banner />
       <Explore />
       <NewArrival />
       <ChooseUs />
       <ProductHighlight />
       <BrowseCategory />
-      <NoHeader /> */}
-      <ShoppingCart/>
+      <NoHeader />
+      {/* <ShoppingCart/> */}
       <Review />
       <Blogs />
     </>
