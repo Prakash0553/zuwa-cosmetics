f2195b217823b2edeb37dcb70ffbb8f2c84867b0
Prakash Giri <prakashgiri20540811@gmail.com>
2026-08-31 08:21:28 +0545

    refactor NoHeader component to use dynamic product data from API

--- client/src/components/NoHeader.jsx
+++ client/src/components/NoHeader.jsx
@@ -1,33 +1,12 @@
 import { FaCartShopping } from "react-icons/fa6";
-import rect1 from "../assets/rect1.webp";
-import rect2 from "../assets/rect2.webp";
-import rect3 from "../assets/rect3.webp";
-import rect4 from "../assets/rect4.webp";
-
-const products = [
-  {
-    image: rect1,
-    category: "Retinol + Hyaluronic Acid Face Serum - 30ml",
-    products: 5,
-  },
-  {
-    image: rect2,
-    category: "Kojic Acid Sunscreen Lotion SPF 50-200 ml",
-    products: 8,
-  },
-  {
-    image: rect3,
-    category: "Kojic acid 2% serum 30 ml",
-    products: 6,
-  },
-  {
-    image: rect4,
-    category: "Sophia Brightening Facial Serum 30 ml",
-    products: 4,
-  },
-];
+import { useProducts } from "../hooks/useProducts";
 
 const NoHeader = () => {
+  const {data: products, isLoading, error} = useProducts()
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
+
   return (
     <div className="w-full bg-[#991b60]">
       <div className="py-16 grid grid-cols-1 lg:grid-cols-2 gap-8 px-5">
@@ -35,18 +14,18 @@ const NoHeader = () => {
         <div key={index} className="">
 
           {/* Image */}
-          <div className="w-full ">
+          <div className="w-full sm:h-[250px] md:-h-[320px] lg:h-[380px]">
             <img
               src={product.image}
               alt={product.category}
-              className="w-full rounded-2xl"
+              className="w-full rounded-2xl h-full object-cover"
             />
           </div>
 
           {/* Bottom */}
           <div className="flex items-center justify-between px-2 mt-3 gap-2 ">
             <h3 className="text-xl  font-semibold text-white">
-              {product.category}
+              {product.category.name}
             </h3>
 
             <button
